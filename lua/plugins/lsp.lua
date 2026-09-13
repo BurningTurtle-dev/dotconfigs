dafa42beff0d6254e0802337d46ac4d815b2d1d8
BurningTurtle <git@burningturtle.win>
2026-09-14 01:39:20 +0200

    fixes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -41,12 +41,19 @@ return {
 			-- for nix, only on nix machines
 			if vim.fn.executable("nixd") == 1 then
 				table.insert(servers, "nixd")
+
+				vim.lsp.config("clangd", {
+					cmd = {
+						"clangd",
+						"--query-driver=/etc/profiles/per-user/*/bin/gcc,/etc/profiles/per-user/*/bin/cc,/run/current-system/sw/bin/*",
+					},
+				})
 			end
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			for _, server in ipairs(servers) do
-				vim.lsp.config(server, {})
+				vim.lsp.config(server, { capabilities = capabilities })
 			end
 
 			vim.lsp.enable(servers)
