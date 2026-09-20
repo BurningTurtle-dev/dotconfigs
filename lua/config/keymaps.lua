14cf95b45b5908291648b847b59c8ec4993e4464
BurningTurtle <git@burningturtle.win>
2026-09-20 21:26:01 +0200

    shift j/k moves half screen and centers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,9 +23,18 @@ map("n", "<leader>w", "<cmd>bd<CR>", { desc = "Closes current buffer" })
 map("n", "<leader>q", "<cmd>bd!<CR>", { desc = "Closes current buffer" })
 
 -- Clipboard-safe delete (black hole register)
-map({ "n", "v" }, "d", '""d', { desc = "Delete without touching system clipboard" })
-map("n", "D", '""D', { desc = "Delete to end of line without touching system clipboard" })
-map("n", "dd", '""dd', { desc = "Delete line without touching system clipboard" })
+map({ "n", "v" }, "d", '"_d', { desc = "Delete without touching system clipboard" })
+map("n", "D", '"_D', { desc = "Delete to end of line without touching system clipboard" })
+map("n", "yd", "dd", { desc = "Yank and delete line" })
 
 -- Terminal mode
 map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Half-page scroll, centered
+map("n", "<S-j>", "<C-d>zz", { desc = "Half page down, centered" })
+map("n", "<S-k>", "<C-u>zz", { desc = "Half page up, centered" })
+map("n", "<S-Down>", "<C-d>zz", { desc = "Half page down, centered" })
+map("n", "<S-Up>", "<C-u>zz", { desc = "Half page up, centered" })
+
+map("v", "<S-j>", "<C-d>zz", { desc = "Half page down, centered" })
+map("v", "<S-k>", "<C-u>zz", { desc = "Half page up, centered" })
